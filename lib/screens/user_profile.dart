791defcaa4c963fb484ea2657d0bac95d0efd528
yaniarrani <115916449+yaniarrani@users.noreply.github.com>
2022-12-19 00:55:03 +0700

    refactor: tbtb g bs connect, need fix but im too lazy rn, maybe next time

--- lib/screens/user_profile.dart
+++ lib/screens/user_profile.dart
@@ -1,3 +1,5 @@
+// still need improvement hshshshs
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:ujikom_application/background/color_background.dart';
